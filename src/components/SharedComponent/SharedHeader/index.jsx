34390fd4eb0c9807f54f7bba56510fb66a76b13f
shivang <shivang.ramola.hp@gmail.com>
2024-05-11 14:52:58 +0530

    fix(src): sad

--- src/components/SharedComponent/SharedHeader/index.jsx
+++ src/components/SharedComponent/SharedHeader/index.jsx
@@ -1,3 +1,13 @@
 export const SharedHeader = () => {
-  return <div>index</div>;
+  return (
+    <header className="py-5">
+      <div className="w-11/12 mx-auto">as</div>
+      <div className="py-10">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        nemo repellat dolor quasi odit totam iure rem adipisci. Amet in
+        excepturi sunt fugiat laudantium iusto explicabo ducimus doloribus est
+        ratione?
+      </div>
+    </header>
+  );
 };
