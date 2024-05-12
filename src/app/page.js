f6e9682a37cf5182f156739f8193f8060de9804c
Rajul-Tiwari <rajul.tiwari.hp@gmail.com>
2024-05-12 13:02:39 +0530

    added count

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import LetsCreate from "@/components/letscreate";
+import NumberTalks from "@/components/numbertalks";
 import UserRecommend from "@/components/ourClient";
 import Image from "next/image";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <UserRecommend />
       <LetsCreate />
+      <NumberTalks />
     </div>
   );
 }
