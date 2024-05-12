10c56c6ba22d33cbb9b9e8cc9d26db9a6f1a7ab1
shivang <shivang.ramola.hp@gmail.com>
2024-05-12 16:41:51 +0530

    feat(src): na
    na

--- src/components/SharedComponent/SharedFooter/index.jsx
+++ src/components/SharedComponent/SharedFooter/index.jsx
@@ -1,75 +1,110 @@
 import { logo } from "@/assets";
-import { footerTitle, socialNetwork } from "@/constant";
+import { GoArrowUpRight } from "react-icons/go";
+import {
+  footerNewsletter,
+  footerOfficeInfo,
+  footerTitle,
+  socialNetwork,
+} from "@/constant";
 import Link from "next/link";
+import { IoArrowRedoOutline } from "react-icons/io5";
+import { RedLine } from "..";
 
 export const SharedFooter = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <footer className="bg-primaryBlack py-4">
-      <div className="w-11/12 mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-8 py-16 text-white">
+    <footer className="bg-primaryBlack">
+      <div className="w-11/12 mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-12 py-16">
         <div>
-          <h5 className="text-xl">PromoDaddy Digital</h5>
-          <div className="h-[70px] w-[70px] mt-5">
-            <img src={logo.src} alt="logo" className="h-full w-full" />
+          <h1 className="text-xl">PromoDaddy Digital</h1>
+          <div className="w-[80px] h-[80px] mt-8">
+            <img src={logo.src} className="h-full w-full" alt="logo" />
           </div>
-          <p className="text-sm mt-5">
-            Creates and manages targeted online ads on Google Search and partner
-            websites
+          <div className="mt-5 relative">
+            <GoArrowUpRight className="h-8 w-8 absolute top-2 right-1 text-black " />
+            <input
+              type="email"
+              placeholder="Your email here"
+              className="text-lg text-black py-3 px-3 rounded-lg w-full outline-none"
+            />
+          </div>
+          <p className="text-sm mt-4 leading-5">
+            By signing up to receive emails from Motto, you agree to our Privacy
+            Policy. We treat your info responsibly.
           </p>
-          <h5 className="text-white font-light mt-5">Social Network</h5>
-          <div className="flex gap-2 mt-6">
-            {socialNetwork.map((logo, index) => (
-              <a
+        </div>
+        <div>
+          <h6 className="text-primaryWhite text-23 font-bold leading-30">
+            {footerTitle?.linkOne.title}
+          </h6>
+          <div className="mt-0 mb-8">
+            <RedLine />
+          </div>
+          <div className="space-y-2">
+            {footerTitle?.linkOne.content.map((content, index) => (
+              <Link
                 key={index}
-                className="rounded-full p-3 cursor-pointer"
-                href={logo.link}
+                href={content.link}
+                className="block text-base w-fit transition-all duration-300 ease-in-out"
               >
-                <img
-                  src={logo?.image.src}
-                  className="h-4 w-4"
-                  alt={logo.link}
-                />
+                <span className="group relative inline-block">
+                  <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out py-1">
+                    {content.name}
+                  </span>
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h6 className="text-primaryWhite text-23 font-bold leading-30">
+            {footerTitle?.linkTwo.title}
+          </h6>
+          <div className="mt-0 mb-8">
+            <RedLine />
+          </div>
+          <div className="space-y-2">
+            {footerTitle?.linkTwo.content.map((content, index) => (
+              <Link
+                key={index}
+                href={content.link}
+                className="block text-base w-fit transition-all duration-300 ease-in-out"
+              >
+                <span className="group relative inline-block">
+                  <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out py-1">
+                    {content.name}
+                  </span>
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="text-primaryWhite font-bold leading-30">
+            <h6 className="text-primaryWhite text-23 font-bold leading-30">
+              Contact
+            </h6>
+            <div className="mt-0 mb-8">
+              <RedLine />
+            </div>
+            <p>Plot no. 26, mahaveer nagar vistar yojana, kota, 324005</p>
+            <div className="mt-8">
+              <a className="block" href="">
+                +91 9602089182
               </a>
-            ))}
-          </div>
-        </div>
-        <div>
-          <h5 className="text-xl">{footerTitle.linkOne.title}</h5>
-          <div className="mt-5 space-y-3">
-            {footerTitle?.linkOne?.content.map((con, index) => (
-              <Link key={index} href={con.link} className="block text-base">
-                {con.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div>
-          <h5 className="text-xl">{footerTitle.linkTwo.title}</h5>
-          <div className="mt-5 space-y-3">
-            {footerTitle?.linkTwo?.content.map((con, index) => (
-              <Link key={index} href={con.link} className="block text-base">
-                {con.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div>
-          <h5 className="text-xl">Contact</h5>
-          <p className="mt-5">
-            Plot no. 26, mahaveer nagar vistar yojana, kota, 324005
-          </p>
-          <div className="mt-5">
-            <a href="" className="block">
-              +91 9602089182
-            </a>
-            <a href="" className="block">
-              promodaddydigital@gmail.com
-            </a>
+              <a className="block" href="">
+                promodaddydigital@gmail.com
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
       <div className="w-11/12 mx-auto py-7 border-t border-brown ">
-        <p className="text-center font-light text-white">
+        <p className="text-center text-14 font-light text-primaryWhite">
           © 2024 PromoDaddy Digital. All rights reserved.
         </p>
       </div>
--- src/components/SharedComponent/SharedHeader/index.jsx
+++ src/components/SharedComponent/SharedHeader/index.jsx
@@ -7,27 +7,31 @@ import { MobileDrawer } from "@/components/MobileDrawer";
 import { useState } from "react";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
+import { usePathname } from "next/navigation";
 
 export const SharedHeader = () => {
+  const path = usePathname();
   const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
   };
 
   return (
-    <header className="py-5 bg-primaryBlack">
+    <header className="py-5 bg-primaryBlack sticky top-0 z-[999]">
       <div className="w-11/12 mx-auto">
         <div className="flex items-center justify-between">
           <div className="h-[70px] w-[70px]">
             <img src={logo.src} alt="logo" className="h-full w-full" />
           </div>
-          <div className="hidden lg:flex items-center gap-[150px] justify-around">
+          <div className="hidden lg:flex items-center gap-[140px] justify-around">
             <div className="flex items-center gap-10">
               {navTitle.map((nav, index) => (
                 <Link
                   href={nav.link}
                   key={index}
-                  className="w-fit font-bold text-white text-sm"
+                  className={`w-fit font-bold text-white text-sm ${
+                    path === nav.link ? "text-primary" : null
+                  }`}
                 >
                   {nav.name}
                 </Link>
@@ -36,9 +40,9 @@ export const SharedHeader = () => {
             <div>
               <SharedButton
                 text="WORKS WITH US"
-                className="text-white font-bold text-sm bg-primaryBlue px-4 py-2 rounded-lg"
+                className="text-white font-bold text-sm bg-primaryBlue px-4 py-1 rounded-lg"
               >
-                Start{" "}
+                WORKS WITH US
               </SharedButton>
             </div>
           </div>
