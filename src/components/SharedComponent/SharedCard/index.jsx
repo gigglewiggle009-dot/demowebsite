65dd9600402a32468bb2f5a290a101b9e7e0da04
Rajul-Tiwari <rajul.tiwari.hp@gmail.com>
2024-05-15 17:03:31 +0530

    increase font size

--- src/components/SharedComponent/SharedCard/index.jsx
+++ src/components/SharedComponent/SharedCard/index.jsx
@@ -15,8 +15,8 @@ export const SharedCard = ({ item, director, management }) => {
                     </>
                 ) : (
                     <>
-                        <h1 className='text-base font-semibold'>{item?.header}</h1>
-                        <p className='text-sm mt-4'>{item?.text}</p>
+                        <h1 className='text-xl font-semibold text-blue-200'>{item?.header}</h1>
+                        <p className='text-lg mt-4 text-gray-400'>{item?.text}</p>
                     </>
                 )}
                 {/* Management Condition */}
