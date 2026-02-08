322fb580b24380167cafee540dffdf17d6f30313
Promo Daddy <gigglewiggle009@gmail.com>
2026-02-08 22:27:51 +0530

    broken link code setting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const webpack = require('webpack');
 
 const nextConfig = {
@@ -12,6 +13,48 @@ const nextConfig = {
   // Adding the standalone output configuration
   output: "standalone",
 
+=======
+const webpack = require("webpack");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    unoptimized: true,
+    domains: [],
+    formats: ["image/avif", "image/webp"],
+  },
+
+  output: "standalone",
+
+  async redirects() {
+    return [
+      {
+        source: "/service",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/projects",
+        destination: "/project",
+        permanent: true,
+      },
+      {
+        source: "/about",
+        destination: "/aboutus",
+        permanent: true,
+      },
+      {
+        source: "/clients",
+        destination: "/client",
+        permanent: true,
+      },
+    ];
+  },
+
+>>>>>>> e3da751 (Add latest code)
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -23,7 +66,11 @@ const nextConfig = {
         $: "jquery",
         jQuery: "jquery",
         "window.jQuery": "jquery",
+<<<<<<< HEAD
       }),
+=======
+      })
+>>>>>>> e3da751 (Add latest code)
     );
 
     return config;
